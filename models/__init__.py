cbfaa15e36b461d747e7dacec691e0e061a55ab0
binikiya <40537151+binikiya@users.noreply.github.com>
2021-12-22 00:44:38 -0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-This servers as a reference for storage in the module
-"""
-
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
