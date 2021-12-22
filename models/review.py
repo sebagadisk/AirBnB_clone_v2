5268c69652edb1cdd989a694183f6f9fb06e353c
binikiya <biniyamkiya3682@gamil.com>
2021-12-22 07:37:06 -0800

    AirBnB clone commit

--- models/review.py
+++ models/review.py
@@ -2,12 +2,27 @@
 """
 Describes Review class
 """
-from models.base_model import BaseModel
+
+from models.base_model import BaseModel, Base
+import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """Represents Review:"""
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
