8e63e4518a8879c20f49bac7d1624fc9dafcca50
binikiya <biniyamkiya3682@gamil.com>
2021-12-23 03:36:01 -0800

    AirBnB clone commit

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initializes the basemodel class"""
         if not kwargs:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
             models.storage.new(self)
