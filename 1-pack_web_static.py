63c89897a6ae64838aa3d7c5f16c1b1ed425e6c1
binikiya <biniyamkiya3682@gamil.com>
2021-12-23 02:06:19 -0800

    AirBnB clone commit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,29 @@
-#!/usr/bin/env bash
-# sets the web server for the deployement
+#!/usr/bin/python3
+"""
+script based on set_static_web.sh that distributes an
+archive to web servers
+"""
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y intall nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Testers" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['34.44.127.228', '34.44.206.195']
+
+def do_deploy(archive_path):
+    """destributes an archive to web server"""
+    if exists(archive_path):
+        return False
+    try:
+        files = archive_path.split("/")[-1]
+        exten = files.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, exten))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(files, path, exten))
+        run('rm /tmp/{}'.format(files))
+        run('mv {0][1]/web_static/* {0}{1}/'.format(path, exten))
+        run('rm -rf {}{}/web_static'.format(path, exten))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, exten))
+        return True
+    except:
+        return False
